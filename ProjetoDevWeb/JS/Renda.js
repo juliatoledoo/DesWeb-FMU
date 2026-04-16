2840bb313fa5e7aee7a62a63240c8a57224045f0
Júlia Toledo <contatojuliamt@gmail.com>
2026-04-16 16:00:02 -0300

    Update Renda.js

--- ProjetoDevWeb/JS/Renda.js
+++ ProjetoDevWeb/JS/Renda.js
@@ -1,3 +1,26 @@
+function calcularINSS(salario) {
+    const tipo = localStorage.getItem("tipoContrato");
+    
+    // Só calcula se for CLT
+    if (tipo !== "clt") return 0;
+
+    let inss = 0;
+
+    if (salario <= 1412.00) {
+        inss = salario * 0.075;
+    } else if (salario <= 2666.68) {
+        inss = (1412.00 * 0.075) + ((salario - 1412.00) * 0.09);
+    } else if (salario <= 4000.03) {
+        inss = (1412.00 * 0.075) + (1254.68 * 0.09) + ((salario - 2666.68) * 0.12);
+    } else if (salario <= 7786.02) {
+        inss = (1412.00 * 0.075) + (1254.68 * 0.09) + (1333.35 * 0.12) + ((salario - 4000.03) * 0.14);
+    } else {
+        inss = 908.85; // Teto máximo do INSS
+    }
+
+    return inss;
+}
+
 function formatarMoeda(input) {
     let valor = input.value.replace(/\D/g, ""); 
     valor = (valor / 100).toFixed(2) + "";
